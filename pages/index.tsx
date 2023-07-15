9f47be7d921cd11f0e6876ab69edf56cf64d7bfe
Chris Short <13677134+devcshort@users.noreply.github.com>
2023-07-15 11:19:57 -0700

    static export and updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
 import { Header } from '@/components/Header';
 import { Box, Container, Heading, Stack, Text } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import { glob } from 'glob';
 import Head from 'next/head';
+import Image from 'next/image';
+import path from 'path';
 
-export default function Home() {
+type Props = {
+  images: string[];
+  metaFiles: string[];
+};
+
+const regex = /^(.*)(?=\d{4}\/\d{1,2}\/\d{2})/;
+export async function getStaticProps() {
+  const today = dayjs();
+  const imagePath = path.resolve('./public/archive');
+  const pathForToday = `${today.year()}/${today.month() + 1}/${today.date()}`;
+
+  const images = await glob(`${imagePath}/${pathForToday}/*`, {
+    ignore: {
+      ignored: (p) => /\.meta.json$/.test(p.name),
+    },
+  });
+
+  const metaFiles = await glob(`${imagePath}/${pathForToday}/*.meta.json`);
+
+  return {
+    props: {
+      images: images.map((image: string) => image.replace(regex, '')),
+      metaFiles: metaFiles.map((metaFile: string) =>
+        metaFile.replace(regex, '')
+      ),
+    },
+  };
+}
+
+export default function Home({ images, metaFiles }: Props) {
   return (
     <>
       <Head>
@@ -16,6 +49,18 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <Container maxW="container.xl" as="main" pt={8} pb={8} pl={8} pr={8}>
+        {images.map((image) => (
+          <Image
+            key={image}
+            src={`/archive/${image}`}
+            alt=""
+            width={200}
+            height={600}
+          />
+        ))}
+      </Container>
     </>
   );
 }
